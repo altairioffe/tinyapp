08cccb572a778188904b2e79022458854a837707
Altair Ioffe <altair.ioffe@gmail.com>
2020-02-19 00:59:38 +0000

    cleaned up dead code

--- express_server.js
+++ express_server.js
@@ -25,35 +25,30 @@ const urlDataBase = {
   "9sm5xK": "http://www.getindezone.com"
 };
 
-//const userName = {}
-
 app.get("/", (req, res) => {
   res.send('GREETINGS');
 });
 
 app.get("/urls.json", (req, res) => {
-  res.json(urlDataBase)
-
-})
+  res.json(urlDataBase);
+});
 
 app.post("/login", (req, res) => {
-  const username = req.body.username
-  log(username)
-  res.cookie('username', username)
-  //userName.username = username;
-  //log(cookie)
-  res.redirect("/urls")
-})
+  const username = req.body.username;
+  log(username);
+  res.cookie('username', username);
+  res.redirect("/urls");
+});
 
 app.post("/logout", (req, res) => {
   res.clearCookie('username');
   res.redirect("/urls");
-})
+});
 
 
 app.get("/hello", (req, res) => {
-  res.send("<html><body> Greetings, <b>cyber</b>traveller</body></html>\n")
-})
+  res.send("<html><body> Greetings, <b>cyber</b>traveller</body></html>\n");
+});
 
 app.get("/urls/new", (req, res) => {
   let templateVars = { username: req.cookies.username };
@@ -63,13 +58,12 @@ app.get("/urls/new", (req, res) => {
 
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDataBase, username: req.cookies.username };
-  //log(templateVars)
   res.render("urls_index", templateVars);
 });
 
 
 app.get("/urls/:shortURL", (req, res) => {
-  const shortURL = req.params.shortURL
+  const shortURL = req.params.shortURL;
   let templateVars = { shortURL: shortURL, longURL: urlDataBase[shortURL], username: req.cookies.username };
   res.render("urls_show", templateVars);
 });
@@ -77,29 +71,27 @@ app.get("/urls/:shortURL", (req, res) => {
 app.post("/urls/:shortURL/update", (req, res) => {
   const shortURL = req.params.shortURL;
   const newURL = req.body.newURL;
-  log(newURL)
   urlDataBase[shortURL] = newURL;
-  log(urlDataBase);
   res.redirect("/urls");
 });
 
 app.post("/urls/:shortURL/delete", (req, res) => {
   const shortURL = req.params.shortURL
   delete urlDataBase[shortURL];
-  res.redirect("/urls")
+  res.redirect("/urls");
 })
 
 app.post("/urls", (req, res) => {
 
   const longURL = req.body.longURL;
   const shortURL = generateRandomString();
-  const redirect = `/urls/${shortURL}`
+  const redirect = `/urls/${shortURL}`;
   urlDataBase[shortURL] = longURL;
-  res.redirect(redirect)
+  res.redirect(redirect);
 });
 
 app.get("/u/:shortURL", (req, res) => {
-  const shortURL = req.params.shortURL
+  const shortURL = req.params.shortURL;
   const longURL = urlDataBase[shortURL];
   res.redirect(longURL);
 });
